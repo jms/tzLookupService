65a0e1e336997f6ed314f34d94e9279f47a5ddb3
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2020-05-14 21:45:48 -0600

    demo project go/gin for tz lookup

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/jms/tzLookupService
+
+go 1.14
+
+require (
+	github.com/evanoberholster/timezoneLookup v0.0.0-20190926092755-58040732034d
+	github.com/gin-gonic/gin v1.6.3
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	go.etcd.io/bbolt v1.3.4 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"log"
+	"net/http"
+)
+import timezone "github.com/evanoberholster/timezoneLookup"
+
+var tz timezone.TimezoneInterface
+
+type Location struct {
+	Lat float32 `form:"lat" binding:"required"`
+	Lon float32 `form:"lon" binding:"required"`
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/tz", func(c *gin.Context) {
+		var location Location
+		if err := c.ShouldBindWith(&location, binding.Query); err == nil {
+			res, err := lookupTz(location.Lat, location.Lon)
+			if err != nil {
+				c.JSON(http.StatusNotFound, gin.H{"error": "timezone not found"})
+			}
+			c.JSON(http.StatusOK, gin.H{"timezone": res})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+	})
+	r.Run()
+}
+
+func lookupTz(Lat float32, Lon float32) (string, error) {
+	tz, err := timezone.LoadTimezones(timezone.Config{
+		DatabaseType: "boltdb",   // memory or boltdb
+		DatabaseName: "timezone", // Name without suffix
+		Snappy:       true,
+		Encoding:     "msgpack", // json or msgpack
+	})
+	if err != nil {
+		log.Println(err)
+	}
+	res, err := tz.Query(timezone.Coord{Lat: Lat, Lon: Lon})
+	tz.Close()
+
+	if err != nil {
+		log.Println(err)
+	}
+	return res, err
+}
